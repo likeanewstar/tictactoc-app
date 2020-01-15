7c6f84bb03dff43e00224697497495777c096d1b
unknown <kangsaebyeolgmail.com>
2020-01-15 13:10:29 +0900

    Lifting state up

--- src/index.js
+++ src/index.js
@@ -3,22 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-    constructor(props) {
+    /* constructor(props) {
       super(props); // JavaScript 클래스에서 하위 클래스의 생성자를 정의할 때 항상 super를 호출해야합니다. 모든 React 컴포넌트 클래스는 생성자를 가질 때 super(props) 호출 구문부터 작성해야 합니다.
       
       // 무언가를 “기억하기”위해 component는 state를 사용합니다.
       this.state = {
         value: null
       };
-    }
+    } */
+    // Square은 게임의 상태를 유지할 필요가 없기 때문에 constructor를 삭제
 
     render() {
       return (
         <button 
           className="square" 
-          onClick={() => this.setState({value: 'X'})} // Square의 render 함수 내부에서 onClick 핸들러를 통해 this.setState를 호출하는 것으로 React에게 <button>을 클릭할 때 Square가 다시 렌더링해야 한다고 알릴 수 있습니다.
+          onClick={() => this.props.onClick({value: 'X'})} // Square의 render 함수 내부에서 onClick 핸들러를 통해 this.setState를 호출하는 것으로 React에게 <button>을 클릭할 때 Square가 다시 렌더링해야 한다고 알릴 수 있습니다.
         >
-          {this.state.value}
+          {this.props.value}
         </button> // 업데이트 이후에 {this.state.value}가 X로 변화.
       );
     }
@@ -33,7 +34,11 @@ class Square extends React.Component {
     }
     
     renderSquare(i) {
-      return <Square value={this.state.squares[i]}/>;
+      return <Square 
+              value={this.state.squares[i]}
+              onClick={() => this.handleClick(i)}
+              // 컴포넌트는 자신이 정의한 state에만 접근할 수 있으므로 Square에서 Board의 state를 직접 변경할 수 없습니다. 대신에 Board에서 Square로 함수를 전달하고 Square는 사각형을 클릭할 때 함수를 호출할 것입니다. 
+              />;
     }
   
     render() {
